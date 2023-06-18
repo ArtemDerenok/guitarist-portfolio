e32b74573d7cd26ddac19aa164d17bdf7ace57c0
ArtemDerenok <derenokartem95@gmail.com>
2023-06-18 11:26:41 +0300

    feat: final

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".nav-menu"),
+    menuItem = document.querySelectorAll(".nav-menu__list-item"),
+    hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("nav-menu_active");
+  });
+
+  menu.addEventListener("click", () => {
+    hamburger.classList.remove("hamburger_active");
+    menu.classList.remove("nav-menu_active");
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("nav-menu_active");
+    });
+  });
+});
